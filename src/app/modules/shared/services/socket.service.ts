34ce753bae89f09fad27860cc21bb112dbd962e0
JoseCruzAvila <jose.cruz@sofka.com.co>
2022-07-03 10:05:34 -0500

    Merge

--- src/app/modules/shared/services/socket.service.ts
+++ src/app/modules/shared/services/socket.service.ts
@@ -4,6 +4,28 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class SocketService {
+  connected = false;
+  aggregateId: string = "";
+  
+  constructor() {
 
-  constructor() { }
+  }
+
+  ngOnInit(): void {
+  }
+
+  init(aggregateId: string): void {
+    this.aggregateId = aggregateId;
+
+    if (!this.connected) {
+      var aggregateId = this.aggregateId;
+      console.log("Val: " + aggregateId);
+      let socket = new WebSocket("ws://" + location.host + "/retrieve/" + aggregateId);
+
+      socket.onmessage = function (m) {
+        console.log("Got message: " + m.data);
+        //document.getElementById("result").append(m.data + "\n");
+      };
+    }
+  }
 }
