b007fd4be616736caad781fabfbd6cf5269d527b
JoseCruzAvila <jose.cruz@sofka.com.co>
2022-07-04 13:53:12 -0500

    Add animations with angular

---
+++ src/app/modules/shared/models/trackFragment.ts
@@ -0,0 +1,208 @@
+export interface TrackFragment {
+    id: number,
+    metersDistance: number,
+    vwDistance: number,
+    vhDistance: number,
+    angle: number
+}
+
+export let trackFragmentCar1: TrackFragment[] = [
+    {
+        id: 1,
+        metersDistance: 0,
+        vwDistance: 32,
+        vhDistance: 0,
+        angle: 0
+    },
+    {
+        id: 2,
+        metersDistance: 0,
+        vwDistance: 32,
+        vhDistance: -30,
+        angle: -90
+
+    },
+    {
+        id: 3,
+        metersDistance: 0,
+        vwDistance: 52,
+        vhDistance: -30,
+        angle: 0
+    },
+    {
+        id: 4,
+        metersDistance: 0,
+        vwDistance: 52,
+        vhDistance: -68,
+        angle: -90
+    },
+    {
+        id: 5,
+        metersDistance: 0,
+        vwDistance: 13,
+        vhDistance: -68,
+        angle: -180
+    },
+    {
+        id: 6,
+        metersDistance: 0,
+        vwDistance: 13,
+        vhDistance: -40,
+        angle: -270
+    },
+    {
+        id: 7,
+        metersDistance: 0,
+        vwDistance: -22,
+        vhDistance: -40,
+        angle: -180
+    },
+    {
+        id: 8,
+        metersDistance: 0,
+        vwDistance: -22,
+        vhDistance: 0,
+        angle: -360
+    },
+    {
+        id: 9,
+        metersDistance: 0,
+        vwDistance: 0,
+        vhDistance: 0,
+        angle: 0
+    }
+];
+
+export let trackFragmentCar2: TrackFragment[] = [
+    {
+        id: 1,
+        metersDistance: 0,
+        vwDistance: 35,
+        vhDistance: 0,
+        angle: 0
+    },
+    {
+        id: 2,
+        metersDistance: 0,
+        vwDistance: 35,
+        vhDistance: -30,
+        angle: -90
+
+    },
+    {
+        id: 3,
+        metersDistance: 0,
+        vwDistance: 56,
+        vhDistance: -30,
+        angle: 0
+    },
+    {
+        id: 4,
+        metersDistance: 0,
+        vwDistance: 56,
+        vhDistance: -82,
+        angle: -90
+    },
+    {
+        id: 5,
+        metersDistance: 0,
+        vwDistance: 10,
+        vhDistance: -82,
+        angle: -180
+    },
+    {
+        id: 6,
+        metersDistance: 0,
+        vwDistance: 10,
+        vhDistance: -53,
+        angle: -270
+    },
+    {
+        id: 7,
+        metersDistance: 0,
+        vwDistance: -25,
+        vhDistance: -53,
+        angle: -180
+    },
+    {
+        id: 8,
+        metersDistance: 0,
+        vwDistance: -25,
+        vhDistance: 0,
+        angle: -360
+    },
+    {
+        id: 9,
+        metersDistance: 0,
+        vwDistance: 0,
+        vhDistance: 0,
+        angle: 0
+    }
+];
+
+export let trackFragmentCar3: TrackFragment[] = [
+    {
+        id: 1,
+        metersDistance: 0,
+        vwDistance: 29,
+        vhDistance: 0,
+        angle: 0
+    },
+    {
+        id: 2,
+        metersDistance: 0,
+        vwDistance: 29,
+        vhDistance: -30,
+        angle: -90
+
+    },
+    {
+        id: 3,
+        metersDistance: 0,
+        vwDistance: 50,
+        vhDistance: -30,
+        angle: 0
+    },
+    {
+        id: 4,
+        metersDistance: 0,
+        vwDistance: 50,
+        vhDistance: -57,
+        angle: -90
+    },
+    {
+        id: 5,
+        metersDistance: 0,
+        vwDistance: 17,
+        vhDistance: -57,
+        angle: -180
+    },
+    {
+        id: 6,
+        metersDistance: 0,
+        vwDistance: 17,
+        vhDistance: -30,
+        angle: -270
+    },
+    {
+        id: 7,
+        metersDistance: 0,
+        vwDistance: -18,
+        vhDistance: -30,
+        angle: -180
+    },
+    {
+        id: 8,
+        metersDistance: 0,
+        vwDistance: -18,
+        vhDistance: 0,
+        angle: -360
+    },
+    {
+        id: 9,
+        metersDistance: 0,
+        vwDistance: 0,
+        vhDistance: 0,
+        angle: 0
+    }
+]
