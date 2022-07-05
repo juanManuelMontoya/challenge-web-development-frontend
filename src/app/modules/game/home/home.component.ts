a74143ef0f6ad909df1d2cd686959a2fa7d41002
JoseCruzAvila <jose.cruz@sofka.com.co>
2022-07-05 12:17:30 -0500

    Store driver with id

--- src/app/modules/game/home/home.component.ts
+++ src/app/modules/game/home/home.component.ts
@@ -36,9 +36,9 @@ export class HomeComponent implements OnInit {
       next: (res) => {
         let data = JSON.parse(res);
         let cars: Car[] = [
-          new Car(data[0].carroId, 'car1', data[0].conductor[Object.keys(data[0].conductor)[0]], trackFragmentCar1),
-          new Car(data[1].carroId, 'car2', data[1].conductor[Object.keys(data[1].conductor)[0]], trackFragmentCar2),
-          new Car(data[2].carroId, 'car3', data[2].conductor[Object.keys(data[2].conductor)[0]], trackFragmentCar3)
+          new Car(data[0].carroId, 'car1', data[0].conductor, trackFragmentCar1),
+          new Car(data[1].carroId, 'car2', data[1].conductor, trackFragmentCar2),
+          new Car(data[2].carroId, 'car3', data[2].conductor, trackFragmentCar3)
         ]
 
         this.displayService.setCarSubject(cars);
